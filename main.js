a1ad1013fcd79c61836522197887ef418b4ee8fb
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-24 09:04:45 +0300

    Done assignment

--- main.js
+++ main.js
@@ -4,14 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const liker = document.querySelector(".like-glyph")
+const modal = document.getElementById('modal')
 
+  liker.addEventListener('click', () =>{
+       mimicServerCall()
+       .then(response => {
+        console.log(response)
+        handlesResponse() })
+       .catch(err => {
+        console.log(err)
+       handleError()})
+})
 
+function handleError(){
+  modal.classList.remove('hidden')
+  setTimeout(callsBack,3000 )
+}
+function callsBack(){
+  modal.classList.add('hidden')
+}
+function handlesResponse(){
+  liker.classList.toggle('activated-heart')
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(url="http://mimicerver.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
